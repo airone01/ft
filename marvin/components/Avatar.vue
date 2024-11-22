96281826d27f50c91efdebda32260e577c38f6f5
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2024-11-22 17:56:36 +0100

    feat: marvin duck heck yeah

---
+++ marvin/components/Avatar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="avatar">
+    <div class="ring-primary ring-offset-base-100 h-10 rounded-full ring ring-offset-2">
+      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+</template>
---
+++ marvin/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useRoute } from 'nuxt/app';
+
+const route = useRoute()
+</script>
+
+<template>
+  <DiscordCard />
+</template>
