99f307e113d45b23417fa1740304cb2e4d4c8c8e
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2024-11-26 18:46:13 +0100

    feat(quack): add recursive duck

--- quack/src/index.ts
+++ quack/src/index.ts
@@ -2,6 +2,7 @@ import {
   Client, GatewayIntentBits,
 } from 'discord.js';
 import {bot} from './bot.js';
+import {ImageProcessor as _ImageProcessor} from './image.js'; // For linking
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages],
