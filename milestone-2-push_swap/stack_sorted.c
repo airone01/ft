e20ee83921513bc161add1ea9705e7ef05722e26
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2024-12-16 14:15:02 +0100

    fix(push_swap): patch sum

--- milestone-2-push_swap/stack_sorted.c
+++ milestone-2-push_swap/stack_sorted.c
@@ -6,7 +6,7 @@
 /*   By: elagouch <elagouch@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 15:49:56 by elagouch          #+#    #+#             */
-/*   Updated: 2024/12/13 15:55:24 by elagouch         ###   ########.fr       */
+/*   Updated: 2024/12/13 20:06:45 by elagouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@
  */
 ssize_t stack_sorted(t_stack *stack)
 {
-    if (!stack->next)
-        return (1);
-    if (stack->val > stack->next->val)
-        return (0);
-    return (stack_sorted(stack->next));
+    while (stack->next)
+    {
+        if (stack->val < stack->next->val)
+            return(0);
+        stack = stack->next;
+    }
+    return (1);
 }
