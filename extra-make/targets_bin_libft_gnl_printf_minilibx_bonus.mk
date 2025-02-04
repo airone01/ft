e6d87e7748a6e7fa01f2f898fb9094bc7e0a52b7
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2025-02-04 13:55:50 +0100

    feat(fdf): add minilibx

---
+++ extra-make/targets_bin_libft_gnl_printf_minilibx_bonus.mk
@@ -0,0 +1,66 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    targets_bin_libft_gnl_printf_minilibx_bonus        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: elagouch <elagouch@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/01/22 13:28:53 by elagouch          #+#    #+#              #
+#    Updated: 2025/02/04 13:54:47 by elagouch         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# **************************************************************************** #
+#                                   TARGETS                                    #
+# **************************************************************************** #
+# GPM? begin make_target_bin_libft_gnl_bonus
+LIBFT_FLAGS		+= -L$(LIBFT_DIR) -lft
+GNL_FLAGS		+= -L$(GNL_DIR) -l:get_next_line.a
+PRINTF_FLAGS	+= -L$(PRINTF_DIR) -l:libftprintf.a
+MINILIBX_FLAGS	+= -L$(MINILIBX_DIR) -l:libmlx_Linux.a
+
+$(LIBFT): $(SRC_LIBFT)
+	@$(ECHO) "$(MSG)🏗️  Building libft\n"
+	@$(MAKE) -C $(LIBFT_DIR)
+
+$(GNL): $(SRC_GNL)
+	@$(ECHO) "$(MSG)🏗️  Building get_next_line\n"
+	@$(MAKE) -C $(GNL_DIR)
+
+$(PRINTF): $(SRC_PRINTF)
+	@$(ECHO) "$(MSG)🏗️  Building ft_printf\n"
+	@$(MAKE) -C $(PRINTF_DIR)
+
+$(MINILIBX): $(SRC_MINILIBX)
+	@$(ECHO) "$(MSG)🏗️  Building minilibx\n"
+	@$(MAKE) -C $(MINILIBX_DIR)
+
+$(NAME): $(OBJ) | $(LIBFT) $(GNL) $(PRINTF) $(MINILIBX)
+	@$(ECHO) "$(MSG)🏗️  Building $(NAME)\n"
+	@$(CC) $(CFLAGS) -o $@ $^ $(GNL_FLAGS) $(LIBFT_FLAGS) $(PRINTF_FLAGS) $(MINILIBX_FLAGS)
+	@$(ECHO) "$(SUCCESS)\n"
+
+$(BNAME): $(OBJ_BONUS) | $(LIBFT) $(GNL) $(PRINTF) $(MINILIBX)
+	@$(ECHO) "$(MSG)🏗️  Building bonuses for $(NAME)\n"
+	@$(CC) $(CFLAGS) -o $(BNAME) $^ $(GNL_FLAGS) $(LIBFT_FLAGS) $(PRINTF_FLAGS) $(MINILIBX_FLAGS)
+	@touch .bonus
+	@$(ECHO) "$(SUCCESSB)\n"
+
+clean:
+	@$(ECHO) "$(MSG)🧹 Cleaning the project\n"
+	@$(RM) -r $(OUT_DIR)
+	@$(MAKE) -C $(LIBFT_DIR) clean
+	@$(MAKE) -C $(GNL_DIR) clean
+	@$(MAKE) -C $(PRINTF_DIR) clean
+	@$(MAKE) -C $(MINILIBX_DIR) clean
+
+fclean: clean
+	@$(ECHO) "$(MSG)🧹 Fcleaning the project\n"
+	@$(RM) $(NAME) $(BNAME)
+	@$(MAKE) -C $(LIBFT_DIR) fclean
+	@$(MAKE) -C $(GNL_DIR) fclean
+	@$(MAKE) -C $(PRINTF_DIR) fclean
+	@$(MAKE) -C $(MINILIBX_DIR) clean
+
+bonus: $(BNAME)
+# GPM? begin make_target_bin_libft_gnl_bonus
