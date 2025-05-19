7d9e896915032c2732ccee053e238b2dfeac041b
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2025-05-19 15:00:24 +0200

    feat(disco): initialize disco sources
    - Copy disco piscine source
    - Enable git LFS

---
+++ piscine-disco/cell03/ex00bis/background.js
@@ -0,0 +1,12 @@
+bodyElement = document.querySelector('body');
+
+const clickHandler = () => {
+  color = "hsl(" + 360 * Math.random() + ', ' +
+             (25 + 70 * Math.random()) + '%, ' + 
+             (85 + 10 * Math.random()) + '%)';
+  $('body').css('background', color);
+}
+
+$(() => {
+  console.log('Script loaded!');
+})
---
+++ piscine-disco/cell03/ex03bis/index.js
@@ -0,0 +1,106 @@
+const ftListEl = $('#ft_list');
+
+/**
+ * Initializes a new DOM element for a TODO
+ * @param {number} i 
+ * @param {string} text 
+ * @returns jQuery element
+ */
+const newTodoElement = (i, text) => {
+  return $('<div>', {
+    id: `todo_${i}`,
+    class: 'todo_element',
+    text: text ?? 'INVALID',
+    click: () => delTodoElement(i)
+  });
+}
+
+/**
+ * Moves all element by one seat backward from the element index passed
+ * @param {number} from 
+ */
+const moveAllBackwardFrom = (from) => {
+  let amount = localStorage.length;
+  // Move items backwards, starting from the removed index
+  for (let i = from; i < amount - 1; i++) {
+    const elementText = localStorage.getItem('todo_' + (i + 1));
+    localStorage.setItem('todo_' + i, elementText);
+  }
+}
+
+/**
+ * Moves all element by one seat forward from the element index passed
+ * @param {number} from 
+ */
+const moveAllForwardFrom = (from) => {
+  let amount = localStorage.length;
+  // Start from the last item and move backward
+  for (let i = amount - 1; i >= from; i--) {
+    const elementText = localStorage.getItem('todo_' + i);
+    localStorage.setItem('todo_' + (i + 1), elementText);
+  }
+}
+
+/**
+ * Removes a TODO in the local storage
+ * @param {number} idx 
+ */
+const removeOne = (idx) => {
+  let amount = localStorage.length;
+  moveAllBackwardFrom(idx);
+  localStorage.removeItem('todo_' + (amount - 1));
+}
+
+const delTodoElement = (idx) => {
+  removeOne(idx);
+  updateDom();
+}
+
+/**
+ * Updates the DOM to reflect changes to the local storage
+ */
+const updateDom = () => {
+  // Remove all elements
+  ftListEl.empty();
+
+  let amount = localStorage.length;
+  for (let i = 0; i < amount; i++) {
+    const elementText = localStorage.getItem('todo_' + i);
+    const element = newTodoElement(i, elementText);
+    ftListEl.append(element);
+  }
+}
+
+const addAtTop = (text) => {
+  moveAllForwardFrom(0);
+  localStorage.setItem('todo_0', text);
+}
+
+const askAndAdd = () => {
+  let amount = localStorage.length;
+  const elementText = window.prompt("Nom du TODO", 'Task ' + (amount + 1));
+  addAtTop(elementText);
+  updateDom();
+}
+
+const clearTodo = () => {
+  localStorage.clear();
+  updateDom();
+}
+
+const addTest = () => {
+  clearTodo();
+  addAtTop('"Be yourself; everyone else is already taken." ― Oscar Wilde');
+  addAtTop('"Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe." ― Albert Einstein');
+  addAtTop('"You only live once, but if you do it right, once is enough." ― Mae West');
+  addAtTop('"In three words I can sum up everything I\'ve learned about life: it goes on." ― Robert Frost');
+  addAtTop('"If you tell the truth, you don\'t have to remember anything." ― Mark Twain');
+  addAtTop('"Always forgive your enemies; nothing annoys them so much." ― Oscar Wilde');
+  updateDom();
+}
+
+//////////////////////////////////////////////////////////////////////
+
+$(document).ready(function() {
+  updateDom();
+});
