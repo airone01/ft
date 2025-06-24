92a077b8ed68354ea6a7d5f68f1cde9611258870
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2025-06-24 13:56:24 +0200

    fix(cub3d): remove useless cast

--- milestone-4-cub3d/src/l_mlx_m.c
+++ milestone-4-cub3d/src/l_mlx_m.c
@@ -23,8 +23,8 @@ void	handle_mlx_bonus_features(t_data *data, int keycode)
 
 void	setup_mlx_hooks(t_data *data)
 {
-	mlx_loop_hook(data->mlx, (int (*)())render_frame, data);
-	mlx_hook(data->win, ON_DESTROY, 0, (int (*)())hook_destroy, data);
+	mlx_loop_hook(data->mlx, render_frame, data);
+	mlx_hook(data->win, ON_DESTROY, 0, hook_destroy, data);
 	mlx_hook(data->win, ON_KEYDOWN, 1L << 0, key_press, data);
 	mlx_hook(data->win, ON_KEYUP, 1L << 1, key_release, data);
 }
