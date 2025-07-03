7378fa2639d0eabf7d9d6fb003e1f55dfdc25277
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2025-07-03 13:43:57 +0200

    feat(cpp00): implement ex02 #66

---
+++ milestone-4-cpp00/ex02/Account.cpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <elagouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 19:32:45 by elagouch          #+#    #+#             */
+/*   Updated: 2025/06/30 19:35:26 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <ctime>
+#include <iostream>
+#include <ostream>
+
+// Constructors & destructor
+Account::Account(int initial_deposit)
+    : _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
+  _accountIndex = _nbAccounts;
+  _nbAccounts++;
+  _totalAmount += initial_deposit;
+  Account::_displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created"
+            << std::endl;
+}
+Account::~Account(void) {
+  Account::_displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed"
+            << std::endl;
+}
+
+// Static functions
+void Account::_displayTimestamp(void) {
+    time_t now = time(NULL);
+    struct tm* t = localtime(&now);
+
+    char buffer[20]; // big enough
+    strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", t);
+
+    std::cout << "[" << buffer << "] ";
+}
+void Account::displayAccountsInfos(void) {
+  Account::_displayTimestamp();
+  std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount
+            << ";deposits:" << _totalNbDeposits
+            << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+// Getters
+int Account::getNbAccounts(void) { return _nbAccounts; }
+int Account::getTotalAmount(void) { return _totalAmount; }
+int Account::getNbDeposits(void) { return _totalNbDeposits; }
+int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
+
+// Static vars
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+int Account::_nbAccounts = 0;
+
+void Account::makeDeposit(int deposit) {
+  _amount += deposit;
+  _totalAmount += deposit;
+  _nbDeposits++;
+  _totalNbDeposits++;
+  Account::_displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";p_amount:" << _amount - deposit
+            << ";deposit:" << deposit << ";amount:" << _amount
+            << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+  if (_amount < withdrawal) {
+    Account::_displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount
+              << ";withdrawal:refused" << std::endl;
+    return false;
+  }
+  _amount -= withdrawal;
+  _totalAmount -= withdrawal;
+  _nbWithdrawals++;
+  _totalNbWithdrawals++;
+  Account::_displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";p_amount:" << _amount + withdrawal
+            << ";withdrawal:" << withdrawal << ";amount:" << _amount
+            << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+  return true;
+}
+
+int Account::checkAmount(void) const { return _amount; }
+
+void Account::displayStatus(void) const {
+  Account::_displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";amount:" << _amount
+            << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals
+            << std::endl;
+}
