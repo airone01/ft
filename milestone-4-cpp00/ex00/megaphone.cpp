d25dd021b955e614f8a8d45af1cac0e5a85a8fa7
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2025-08-18 15:03:12 +0200

    fix(cpp00): gedagedigedagedago

--- milestone-4-cpp00/ex00/megaphone.cpp
+++ milestone-4-cpp00/ex00/megaphone.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 {
 	if (argc == 1)
 	{
-		std::cout << "* GEGAGEDIGEDAGEDAGO *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
 
