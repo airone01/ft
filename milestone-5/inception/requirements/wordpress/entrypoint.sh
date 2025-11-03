8de5135dac8ff7a97359aa643b8ee301a5d22bfe
airone01 <21955960+airone01@users.noreply.github.com>
2025-11-03 13:12:42 +0100

    fix(inception): inception stuff idk

--- milestone-5/inception/requirements/wordpress/entrypoint.sh
+++ milestone-5/inception/requirements/wordpress/entrypoint.sh
@@ -12,11 +12,9 @@ if [ ! -f "$WP_CONFIG" ]; then
   export WP_ADMIN_PASSWORD="$(cat /run/secrets/wordpress_admin_password)"
 
   php85 /app/wp-cli config create --dbhost=$INCEPTION_HOST_DB --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --path="$WP_PATH"
-  # php85 /app/wp-cli option update home 'https://localhost:3000' --path="$WP_PATH"
-  # php85 /app/wp-cli option update siteurl 'https://localhost:3000' --path="$WP_PATH"
 
   php85 /app/wp-cli core install \
-    --url='https://localhost' \
+    --url="https://$SITE_NAME" \
     --title="$WP_SITE_NAME" \
     --admin_user="$WP_ADMIN_USER" \
     --admin_password="$WP_ADMIN_PASSWORD" \
