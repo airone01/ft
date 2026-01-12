c6b01d944b509e10f9677c6c83922c07c097779a
airone01 <21955960+airone01@users.noreply.github.com>
2026-01-12 17:07:19 +0100

    feat(cpp07): ex02

---
+++ milestone-5/cpp07/ex02/Array.hpp
@@ -0,0 +1,50 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <stdexcept>
+
+template <typename T> class Array {
+private:
+  T *_elems;
+  unsigned int _size;
+
+public:
+  ~Array<T>() { delete[] _elems; }
+
+  Array<T> &operator=(const Array<T> &other) {
+    if (*this == &other)
+      return *this;
+    delete[] _elems;
+    _size = other._size;
+    _elems = new T[_size];
+    for (unsigned int i = 0; i < _size; i++)
+      _elems[i] = other._elems[i];
+    return *this;
+  }
+
+  Array<T>(const Array<T> &other) : _size(other._size) {
+    _elems = new T[_size];
+    for (unsigned int i = 0; i < _size; i++)
+      _elems[i] = other._elems[i];
+  }
+
+  // hmm O_O
+  Array<T>() : _elems(new T[0]), _size(0) {}
+  Array<T>(unsigned int n) : _size(n) {
+    _elems = new T[n];
+    for (unsigned int i = 0; i < n; i++) {
+      _elems[i] = T();
+    }
+  }
+
+  const T &operator[](unsigned int index) const {
+    if (index >= _size) {
+      throw std::out_of_range("Array index out of bound, exiting");
+    }
+    return _elems[index];
+  }
+
+  unsigned int size() const { return _size; }
+};
+
+#endif // !ARRAY_HPP
---
+++ milestone-5/cpp07/ex02/main.cpp
@@ -0,0 +1,30 @@
+#include "Array.hpp"
+#include <exception>
+#include <iostream>
+
+int main(void) {
+  Array<int> a = Array<int>(10);
+
+  std::cout << "TEST 1: array init" << std::endl;
+  std::cout << "val a@0 is " << a[0] << ", size is " << a.size() << std::endl;
+  std::cout << "(this should not segfault, size should be 10)" << std::endl << std::endl;
+
+  Array<int> b = Array<int>(a);
+  // b[2] = 100;
+
+  std::cout << "TEST 2: deep copy" << std::endl;
+  std::cout << "ptr a@2 is " << &a[2] << std::endl;
+  std::cout << "ptr b@2 is " << &b[2] << std::endl;
+  std::cout << "(they should not be equal)" << std::endl << std::endl;
+
+  std::cout << "TEST 3: wrong access" << std::endl;
+  try {
+    const int bruh = a[100];
+    (void)bruh;
+  } catch (const std::exception &e) {
+    std::cerr << "Exception caught (good): " << e.what() << std::endl;
+  }
+  std::cout << "(this should throw)" << std::endl << std::endl;
+
+  return 0;
+}
