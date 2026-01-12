6cad2a7aea5356f995c17832a86157c2d0cb81be
airone01 <21955960+airone01@users.noreply.github.com>
2026-01-12 11:09:56 +0100

    feat(cpp07): ex00

---
+++ milestone-5/cpp07/ex00/template.hpp
@@ -0,0 +1,31 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+/**
+ * @brief Swaps the values of two given parameters.
+ */
+template <typename T> void swap(T &a, T &b) {
+  T tmp = a;
+  a = b;
+  b = tmp;
+}
+
+/**
+ * @brief Compares the two values passed as parameters and returns the smallest
+ * one.
+ * @note If they are equal, it returns the second one.
+ */
+template <typename T> T min(T a, T b) {
+  return (a < b) ? a : b;
+}
+
+/**
+ * @brief Compares the two values passed as parameters and returns the greatest
+ * one.
+ * @note If they are equal, it returns the second one.
+ */
+template <typename T> T max(T a, T b) {
+  return (a > b) ? a : b;
+}
+
+#endif // !TEMPLATE_HPP
