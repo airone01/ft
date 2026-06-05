8e101bef68aa364a3fde94a90935cfd46d93ebdf
airone01 <airone01@proton.me>
2026-06-05 21:34:38 +0200

    chore(libasm): organize sources

--- pcc/libasm/build.zig
+++ pcc/libasm/build.zig
@@ -1,10 +1,7 @@
 const std = @import("std");
 
 const src_dir = "pcc/libasm/src";
-const asm_sources = &[_][]const u8{
-    "ft_strlen.s", "ft_strcpy.s", "ft_strcmp.s",
-    "ft_write.s",  "ft_read.s",   "ft_strdup.s",
-};
+const asm_sources = &[_][]const u8{ "ft_strlen.s", "ft_strcpy.s", "ft_strcmp.s", "ft_write.s", "ft_read.s", "ft_strdup.s", "ft_atoi_base_bonus.s", "ft_list_push_front_bonus.s", "ft_list_remove_if_bonus.s", "ft_list_size_bonus.s", "ft_list_sort_bonus.s" };
 
 fn nasmAtLeast3(b: *std.Build) bool {
     const result = std.process.run(b.allocator, b.graph.io, .{
